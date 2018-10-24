e9754c78e9fafcdfcb7997bbef68ce6c1ed058b7
crazydude123 <32983567+crazydude123@users.noreply.github.com>
2018-10-24 11:00:41 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $db_name = "id7468918_vheal";
-$mysql_username = "baba";
-$mysql_password = "baba@";
-$server_name = "baba";
+$mysql_username = "vhealadmin@vheal12";
+$mysql_password = "Vheal1234@";
+$server_name = "vheal12.mysql.database.azure.com";
 $conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
 ?>
